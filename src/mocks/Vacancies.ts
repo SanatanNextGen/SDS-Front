922cdee0e751156cc4707b20828ae783a5772d03
SanatanNextGen <sanatanexpress9@example.com>
2024-10-15 20:43:30 +0530

    SDS

---
+++ src/mocks/Vacancies.ts
@@ -0,0 +1,191 @@
+export const Vacancies = [
+  {
+    id: 1,
+    title: 'Front-End Developer',
+    techs: [
+      {
+        id: 1,
+        name: 'HTML',
+      },
+      {
+        id: 2,
+        name: 'JavaScript',
+      },
+      {
+        id: 3,
+        name: 'React',
+      },
+    ],
+    category: 'Front-End',
+    description:
+      'If the customer is very smart, he or she will be able to achieve the desired result. Let him run away from the smallest thing. Let there be some small things that are blinded, and we lead them to receive the chosen ones with a gentleness that praises them. The company itself is a very successful company. Who, she herself molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi.  adipisicing elit. Quis, ipsam molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi. Error',
+  },
+  {
+    id: 2,
+    title: 'Front-End Developer',
+    techs: [
+      {
+        id: 1,
+        name: 'HTML',
+      },
+      {
+        id: 2,
+        name: 'JavaScript',
+      },
+      {
+        id: 3,
+        name: 'React',
+      },
+    ],
+    category: 'Front-End',
+    description:
+      'If the customer is very smart, he or she will be able to achieve the desired result. Let him run away from the smallest thing. Let there be some small things that are blinded, and we lead them to receive the chosen ones with a gentleness that praises them. The company itself is a very successful company. Who, she herself molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi.  adipisicing elit. Quis, ipsam molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi. Error',
+  },
+  {
+    id: 3,
+    title: 'Desenvolvedor Full-Stack',
+    techs: [
+      {
+        id: 1,
+        name: 'HTML',
+      },
+      {
+        id: 2,
+        name: 'JavaScript',
+      },
+      {
+        id: 3,
+        name: 'React',
+      },
+    ],
+    category: 'Full-Stack',
+    description:
+      'If the customer is very smart, he or she will be able to achieve the desired result. Let him run away from the smallest thing. Let there be some small things that are blinded, and we lead them to receive the chosen ones with a gentleness that praises them. The company itself is a very successful company. Who, she herself molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi.  adipisicing elit. Quis, ipsam molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi. Error',
+  },
+  {
+    id: 4,
+    title: 'Desenvolvedor Full-Stack',
+    techs: [
+      {
+        id: 1,
+        name: 'HTML',
+      },
+      {
+        id: 2,
+        name: 'JavaScript',
+      },
+      {
+        id: 3,
+        name: 'React',
+      },
+    ],
+    category: 'Full-Stack',
+    description:
+      'If the customer is very smart, he or she will be able to achieve the desired result. Let him run away from the smallest thing. Let there be some small things that are blinded, and we lead them to receive the chosen ones with a gentleness that praises them. The company itself is a very successful company. Who, she herself molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi.  adipisicing elit. Quis, ipsam molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi. Error',
+  },
+  {
+    id: 5,
+    title: 'Desenvolvedor Back-End',
+    techs: [
+      {
+        id: 1,
+        name: 'HTML',
+      },
+      {
+        id: 2,
+        name: 'JavaScript',
+      },
+      {
+        id: 3,
+        name: 'React',
+      },
+    ],
+    category: 'Back-End',
+    description:
+      'If the customer is very smart, he or she will be able to achieve the desired result. Let him run away from the smallest thing. Let there be some small things that are blinded, and we lead them to receive the chosen ones with a gentleness that praises them. The company itself is a very successful company. Who, she herself molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi.  adipisicing elit. Quis, ipsam molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi. Error',
+  },
+  {
+    id: 6,
+    title: 'Desenvolvedor Mobile',
+    techs: [
+      {
+        id: 1,
+        name: 'HTML',
+      },
+      {
+        id: 2,
+        name: 'JavaScript',
+      },
+      {
+        id: 3,
+        name: 'React',
+      },
+    ],
+    category: 'Mobile',
+    description:
+      'If the customer is very smart, he or she will be able to achieve the desired result. Let him run away from the smallest thing. Let there be some small things that are blinded, and we lead them to receive the chosen ones with a gentleness that praises them. The company itself is a very successful company. Who, she herself molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi.  adipisicing elit. Quis, ipsam molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi. Error',
+  },
+  {
+    id: 7,
+    title: 'Desenvolvedor Full-Stack - Talent Bank',
+    techs: [
+      {
+        id: 1,
+        name: 'HTML',
+      },
+      {
+        id: 2,
+        name: 'JavaScript',
+      },
+      {
+        id: 3,
+        name: 'React',
+      },
+    ],
+    category: 'Talent Bank',
+    description:
+      'If the customer is very smart, he or she will be able to achieve the desired result. Let him run away from the smallest thing. Let there be some small things that are blinded, and we lead them to receive the chosen ones with a gentleness that praises them. The company itself is a very successful company. Who, she herself molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi.  adipisicing elit. Quis, ipsam molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi. Error',
+  },
+  {
+    id: 8,
+    title: 'Desenvolvedor UX/UI',
+    techs: [
+      {
+        id: 1,
+        name: 'HTML',
+      },
+      {
+        id: 2,
+        name: 'JavaScript',
+      },
+      {
+        id: 3,
+        name: 'React',
+      },
+    ],
+    category: 'UX/UI',
+    description:
+      'If the customer is very smart, he or she will be able to achieve the desired result. Let him run away from the smallest thing. Let there be some small things that are blinded, and we lead them to receive the chosen ones with a gentleness that praises them. The company itself is a very successful company. Who, she herself molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi.  adipisicing elit. Quis, ipsam molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi. Error',
+  },
+  {
+    id: 9,
+    title: 'Desenvolvedor Back-End',
+    techs: [
+      {
+        id: 1,
+        name: 'HTML',
+      },
+      {
+        id: 2,
+        name: 'JavaScript',
+      },
+      {
+        id: 3,
+        name: 'React',
+      },
+    ],
+    category: 'Back-End',
+    description:
+      'If the customer is very smart, he or she will be able to achieve the desired result. Let him run away from the smallest thing. Let there be some small things that are blinded, and we lead them to receive the chosen ones with a gentleness that praises them. The company itself is a very successful company. Who, she herself molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi.  adipisicing elit. Quis, ipsam molestiae natus quaerat aut totam voluptates, non explicabo rem fuga, provident nihil magni minima saepe optio animi ex quasi. Error',
+  },
+]
